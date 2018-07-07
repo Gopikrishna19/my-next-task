1e8c8f65a7c6a57b66720fc323eca5b0f921898b
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-07-07 13:41:59 -0500

    init build config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devServer: {
+    port: 8080,
+    stats: 'minimal'
+  },
+  devtool: 'source-map',
+  entry: './src',
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        test: /\.js$/
+      }
+    ]
+  },
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, 'public')
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html')
+    })
+  ]
+};
