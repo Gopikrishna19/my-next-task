70fd4f2184abc61845e68a4fdb55439da5c1c793
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-07-08 10:56:57 -0500

    split vendor into different bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,50 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  devServer: {
-    port: 8080,
-    stats: 'minimal'
-  },
-  devtool: 'source-map',
-  entry: './src',
-  module: {
-    rules: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        test: /\.js$/
+module.exports = (env, {mode}) => {
+  const config = {
+    entry: './src',
+    module: {
+      rules: [
+        {
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          test: /\.js$/
+        }
+      ]
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            chunks: 'all',
+            name: 'vendor',
+            test: /node_modules/
+          }
+        }
       }
+    },
+    output: {
+      filename: '[id].index.js',
+      path: path.join(__dirname, 'public')
+    },
+    plugins: [
+      new HTMLWebpackPlugin({
+        template: path.join(__dirname, 'src', 'index.html')
+      }),
+      new CopyWebpackPlugin([
+        {from: 'src/static'}
+      ])
     ]
-  },
-  output: {
-    filename: 'app.js',
-    path: path.join(__dirname, 'public')
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html')
-    }),
-    new CopyWebpackPlugin([
-      {from: 'src/static'}
-    ])
-  ]
+  };
+
+  if (mode === 'development') {
+    config.devServer = {
+      port: 8080,
+      stats: 'minimal'
+    };
+    config.devtool = 'source-map';
+  }
+
+  return config;
 };
