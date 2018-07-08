aea07814ea824ffdc4b4adb65d3591f46650cd95
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-07-08 10:57:16 -0500

    dynamically import App when logged in

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {App} from './App';
 import {auth} from './repository/firebase';
 
-auth().onAuthStateChanged(user => {
+auth().onAuthStateChanged(async user => {
   if (user && user.emailVerified) {
+    const {App} = await import('./App');
+
     render(<App/>, document.getElementById('app'));
   } else {
     const authProvider = new auth.GoogleAuthProvider();
