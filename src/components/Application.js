e11faa6e23da6c78ac1227ca15f0a8ce9e3b9312
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-07-08 17:18:01 -0500

    style application page

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import {auth} from '../firebase';
+import styles from '../styles/components/Application.scss';
+import {AppShell} from './AppShell';
+import {Logout} from './icons/Logout';
 
 const handleClick = () => auth().signOut();
 
 export const Application = () =>
-  <div>
-    <button onClick={handleClick}>Logout</button>
-  </div>;
+  <AppShell
+    className={styles.application}
+    controls={[
+      <button
+        className={styles.logoutButton}
+        key='logout'
+        onClick={handleClick}
+      >
+        <Logout/>
+      </button>
+    ]}
+    elevated={false}
+  >
+    <ul className={styles.menu}>
+      <li>
+        <a
+          className={styles.menuLink}
+          href="#">
+          Shopping List
+        </a>
+      </li>
+      <li>
+        <a
+          className={styles.menuLink}
+          href="#">
+          Todo List
+        </a>
+      </li>
+    </ul>
+  </AppShell>;
