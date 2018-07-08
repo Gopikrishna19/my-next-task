cd6ddfd183a63f5783e8dba65e7744520a3918bc
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-07-08 15:21:46 -0500

    style login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import {auth} from '../firebase';
+import styles from '../styles/Login.scss';
 
 const handleClick = () => {
   const authProvider = new auth.GoogleAuthProvider();
 
-  auth().signInWithPopup(authProvider);
+  auth()
+    .signInWithPopup(authProvider)
+    .catch(() => null);
 };
 
 export const Login = () =>
-  <div>
-    <button onClick={handleClick}>Login</button>
+  <div className={styles.login}>
+    <button
+      className={styles.loginButton}
+      onClick={handleClick}>Login
+    </button>
   </div>;
