9265e94a59be7037164e5282ef7f05efa3a303b6
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-07-09 23:35:16 -0500

    use a router switch so it renders only the first matched route

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {Application} from './Application';
 import {ShoppingList} from './ShoppingList';
 
 export const Router = () =>
   <BrowserRouter>
-    <React.Fragment>
+    <Switch>
       <Route exact path='/' component={Application}/>
       <Route exact path='/shopping-list' component={ShoppingList}/>
-    </React.Fragment>
+    </Switch>
   </BrowserRouter>;
