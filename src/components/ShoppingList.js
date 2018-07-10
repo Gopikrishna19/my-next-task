afcc9dd9599643e3e324cd1e85608090e46485fb
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-07-09 22:47:32 -0500

    add Add button to shopping list

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styles from '../styles/components/ShoppingList.scss';
 import {AppShell} from './AppShell';
 import {Card} from './Card';
+import {Add} from './icons/Add';
 
 export const ShoppingList = () =>
   <AppShell
     backButtonLinkTo='/'
+    controls={[
+      <button
+        className={styles.addButton}
+        key='logout'
+      >
+        <Add/>
+      </button>
+    ]}
     elevated={true}
     hasBackButton={true}
   >
---
+++ src/components/icons/Add.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from '../../styles/icons.scss';
+
+export const Add = () =>
+  <svg className={styles.iconBody} viewBox="0 0 24 24">
+    <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
+  </svg>;
