6cb92753e2af5634ab9a26e859f4b28c697eab8a
Gopikrishna Sathyamurthy <sandalblack19@gmail.com>
2018-07-09 22:29:56 -0500

    increase header elevation dynamically for overflowing content

--- src/components/AppShell.js
+++ src/components/AppShell.js
@@ -6,40 +6,72 @@ import {conditionalRequire} from '../utils/props';
 import {Back} from './icons/Back';
 import {Shell} from './icons/Shell';
 
-export const AppShell = props =>
-  <React.Fragment>
-    <header className={`${styles.header} ${props.elevated ? styles.elevated : ''}`}>
-      {
-        props.hasBackButton ?
-          <Link
-            className={styles.backButton}
-            to={props.backButtonLinkTo}
-          >
-            <Back/>
-          </Link> :
-          <Shell/>
-      }
-      <div className={styles.title}>
-        {props.title}
-      </div>
-      {props.controls}
-    </header>
-    <main className={`${props.className} ${styles.content}`}>
-      {props.children}
-    </main>
-  </React.Fragment>;
+const setElevationOnScroll = (element, component) => {
+  element.onscroll = () => {
+    component.setState({elevated: element.scrollTop > 1});
+  };
+};
+
+const setFocus = element => {
+  element.focus();
+};
+
+const setEvents = component => element => {
+  if (element) {
+    setFocus(element);
+    setElevationOnScroll(element, component);
+  }
+};
+
+export class AppShell extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      elevated: false
+    };
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <header className={`${styles.header} ${this.state.elevated ? styles.elevated : ''}`}>
+          {
+            this.props.hasBackButton ?
+              <Link
+                className={styles.backButton}
+                to={this.props.backButtonLinkTo}
+              >
+                <Back/>
+              </Link> :
+              <Shell/>
+          }
+          <div className={styles.title}>
+            {this.props.title}
+          </div>
+          {this.props.controls}
+        </header>
+        <main
+          ref={setEvents(this)}
+          className={`${this.props.className} ${styles.content}`}
+          tabIndex={-1}
+        >
+          {this.props.children}
+        </main>
+      </React.Fragment>
+    );
+  }
+}
 
 AppShell.propTypes = {
   backButtonLinkTo: conditionalRequire(PropTypes.string, props => props.hasBackButton),
   children: PropTypes.any,
   className: PropTypes.string,
   controls: PropTypes.any,
-  elevated: PropTypes.bool,
   hasBackButton: PropTypes.bool,
   title: PropTypes.string
 };
 AppShell.defaultProps = {
-  elevated: false,
   hasBackButton: false,
   title: 'My Next Task'
 };
